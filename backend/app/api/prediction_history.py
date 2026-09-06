7ad41b160fc858757eab85f100a601e967788f01
AYAN0710 <ayankarart1001@gmail.com>
2026-09-06 17:25:38 +0530

    added prediction_history/{prediction_id}

--- backend/app/api/prediction_history.py
+++ backend/app/api/prediction_history.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter,Depends,HTTPException
 from app.core.dependencies import get_current_user
 from app.core.database import predictions_collection
+from bson import ObjectId
 
 router=APIRouter(prefix='/predictions',tags=['Prediction History'])
 
@@ -20,4 +21,31 @@ def get_prediction_history(current_user=Depends(get_current_user)):
         })
     return {
         'predictions':history
+    }
+    
+
+@router.get('/{prediction_id}')
+def get_prediction(prediction_id:str,current_user=Depends(get_current_user)):
+    user_id=str(current_user['_id'])
+    if not ObjectId.is_valid(prediction_id):
+        raise HTTPException(status_code=400,
+                            detail='Invalid prediction id.')
+        
+    prediction=predictions_collection.find_one({
+        '_id':ObjectId(prediction_id),
+        'user_id':user_id
+    })
+    if not prediction:
+        raise HTTPException(
+            status_code=404,
+            detail='Prediction not found.'
+        )
+    
+    return {
+        "prediction_id": str(prediction["_id"]),
+        "animal_type": prediction["animal_type"],
+        "prediction_module": prediction["prediction_module"],
+        "disease": prediction["disease"],
+        "confidence": prediction["confidence"],
+        "created_at": prediction["created_at"]
     }
